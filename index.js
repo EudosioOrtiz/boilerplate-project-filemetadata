90828d916d6ef2adc8318078380fc116b920cf24
Eudosio Ortiz Guillen <163951147+EudosioOrtiz@users.noreply.github.com>
2024-09-26 11:47:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -11,10 +13,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',upload.single('upfile') ,(req,res)=>{
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+/*1. You should provide your own project, not the example URL.
+Waiting:2. You can submit a form that includes a file upload.
+Waiting:3. The form file input field has the name attribute set to upfile.
+Waiting:4. When you submit a file, you receive the file name, type, and 
+size in bytes within the JSON response.*/
